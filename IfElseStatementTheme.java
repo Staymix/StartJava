9744ac65c19ce51181ba7cc4f3490438285c6b85
Staymix <staszubov199530@gmail.com>
2023-01-19 11:47:53 +0600

    для проверки замечаний №2

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -2,24 +2,27 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("1. Перевод псевдокода на язык Java\n");
         int age = 27;
-        boolean man = true;
+        boolean mans = false;
         float hight = 1.88f;
-        char ferstLetter = "Stas".charAt(0);
+        char firstLetter = "Stas".charAt(0);
         if (age > 20) {
                 System.out.println("Тебе больше 20 лет.");
         } else {
             System.out.println("Тебе 20 лет или меньше.");
-        } if (man) {
+        }
+        if (!mans) {
             System.out.println("Ты мужского пола.");
         } else {
             System.out.println("Ты женского пола.");
-        } if (hight > 1.8) {
+        }
+        if (hight > 1.8) {
             System.out.println("Твой рост больше 180 см.");
         } else {
             System.out.println("Твой рост 180 см или меньше.");
-        } if (ferstLetter == 'M') {
+        }
+        if (firstLetter == 'M') {
             System.out.println("Твое имя начинается с буквы М.");
-        } else if (ferstLetter == 'I') {
+        } else if (firstLetter == 'I') {
             System.out.println("Твое имя начинается с буквы I.");
         } else {
             System.out.println("Твое имя не начинается с буквы M или I.");
@@ -41,11 +44,13 @@ public class IfElseStatementTheme {
         boolean isChek = num1 == 0;
         if (isChek) {
             System.out.println("число равно нулю");
-        } if (num1 % 2 == 0 && !isChek) {
+        }
+        if (num1 % 2 == 0 && !isChek) {
             System.out.println("\nявляется четным");
         } else if (!isChek) {
             System.out.println("\nявляется не четным");
-        } if (num1 >= 0 && !isChek) {
+        }
+        if (num1 >= 0 && !isChek) {
             System.out.println("\nявляется положительным");
         } else if (!isChek) {
             System.out.println("\nявляется отрицательным");
@@ -57,9 +62,11 @@ public class IfElseStatementTheme {
         System.out.println("\nИсходные числа " + num1 + " и " + num2);
         if (num1 / 100 == num2 / 100) {
             System.out.println("Одинаковые числа в 3 разряде : " + num1 / 100);
-        } if (num1 % 100 / 10 == num2 % 100 / 10) {
+        }
+        if (num1 % 100 / 10 == num2 % 100 / 10) {
             System.out.println("Одинаковые числа во 2 разряде : " + num1 % 100 / 10);
-        } if (num1 % 10 == num2 % 10) {
+        }
+        if (num1 % 10 == num2 % 10) {
             System.out.println("Одинаковые числа в 1 разряде : " + num1 % 10);
         } else {
             System.out.println("Одинаковых чисел нет");
@@ -127,7 +134,8 @@ public class IfElseStatementTheme {
         } else if (histroryPercent > 91) {
             histroryGrade = 5;
             System.out.println(histroryGrade + " История");
-        } if (programmingPercent <= 60) {
+        }
+        if (programmingPercent <= 60) {
             programmingGrade = 2;
             System.out.println(programmingGrade + " Программирование");
         } else if (programmingPercent > 60 && programmingPercent <= 73) {
@@ -152,7 +160,7 @@ public class IfElseStatementTheme {
         int sumCosts = (sumOfficeRent * 12) + (productionCosts * 12);
         int sumAnnualProfit = (sumSalesAmount * 12) - sumCosts;
         if (sumAnnualProfit > 0) {
-                System.out.println("прибыль за год: " + sumAnnualProfit);
+            System.out.println("прибыль за год: " + sumAnnualProfit);
         } else {
             System.out.println("прибыль за год: " + sumAnnualProfit);
         }
@@ -182,7 +190,8 @@ public class IfElseStatementTheme {
                     " шт на сумму " + billUSD100 * 100 + " USD");
             sumUSD = sumUSD - (billUSD100 * 100);
             billUSD100 = 0;
-        } if ((sumUSD / 10) <= billUSD10 && !isEnough) {
+        }
+        if ((sumUSD / 10) <= billUSD10 && !isEnough) {
             sumUSDtens = sumUSD / 10;
             billUSD10 = billUSD10 - sumUSDtens;
             System.out.println("Банкноты номиналом по 10 USD в количестве " + sumUSDtens + 
@@ -195,7 +204,8 @@ public class IfElseStatementTheme {
                     " шт на сумму " + billUSD10 * 10 + " USD");
             sumUSD = sumUSD - (billUSD10 * 10);
             billUSD10 = 0;
-        } if (sumUSD <= billUSD1 && !isEnough) {
+        }
+        if (sumUSD <= billUSD1 && !isEnough) {
             sumUSDones = sumUSD;
             billUSD1 = billUSD1 - sumUSDones;
             System.out.println("Банкноты номиналом по 1 USD в количестве " + sumUSDones + 
@@ -207,7 +217,8 @@ public class IfElseStatementTheme {
                     " шт на сумму " + billUSD1 + " USD");
             sumUSD = sumUSD - billUSD1;
             billUSD1 = 0;
-        } if (sumUSD == 0) {
+        }
+        if (sumUSD == 0) {
             System.out.println("Купюры выданы на общую сумму " + 
                     (sumUSDones + sumUSDtens + sumUSDHundred + " USD"));
         } else if (sumUSD != 0) {
