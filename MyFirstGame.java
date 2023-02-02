13bdf1504519db13407a70437358ce17c36f6997
Staymix <staszubov199530@gmail.com>
2023-02-02 17:12:23 +0600

    Для проверки №2

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,12 +1,12 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int playNumber = 50;
-        int compyterNumber = 60;
-        while (playNumber != compyterNumber) {
-            if (playNumber > compyterNumber) {
+        int computerNumber = 60;
+        while (playNumber != computerNumber) {
+            if (playNumber > computerNumber) {
                 System.out.println("Число " + playNumber + " больше того, что загадал компьютер");
                 playNumber--;
-            } else if (playNumber < compyterNumber) {
+            } else if (playNumber < computerNumber) {
                 System.out.println("Число " + playNumber + " меньше того, что загадал компьютер");
                 playNumber++;
             }
