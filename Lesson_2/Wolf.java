a59293006625c17ac930b7f7fa3017e7a094e0f3
Staymix <staszubov199530@gmail.com>
2023-02-06 16:26:57 +0600

    Для проверки ДЗ

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -2,26 +2,70 @@ public class Wolf {
     String gender = "Самец";
     String name = "Рекс";
     int weight = 40;
-    int age = 10;
+    int age = 15;
     String color = "Серый";
 
-    void go() {
+    String getGender() {
+        return gender;
+    }
+
+    void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getWeight() {
+        return weight;
+    }
+
+    void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void go() {
         System.out.println("Идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("Воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Охотится");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,6 +1,11 @@
 public class WolfTest {
     public static void  main(String[] args) {
         Wolf wolfOne = new Wolf();
+        wolfOne.setGender("Самка");
+        wolfOne.setName("Джетта");
+        wolfOne.setWeight(30);
+        wolfOne.setAge(10);
+        wolfOne.setColor("Белый");
         System.out.println("Пол - " + wolfOne.gender + "\nКличка - " + wolfOne.name + "\nВес - " +
                 wolfOne.weight + "\nВозраст - " + wolfOne.age + "\nЦвет - " + wolfOne.color);
         wolfOne.go();
