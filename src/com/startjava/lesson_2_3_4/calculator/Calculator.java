d55be404a2257cb94e43c863c2ff25e60b51e3ea
Staymix <staszubov199530@gmail.com>
2023-03-03 15:40:55 +0600

    Для проверки

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,53 +1,46 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.util.Arrays;
-
 public class Calculator {
     private double number1;
     private double number2;
     private char sign;
-    private  String  mathematicalExpression;
-    private String[] data = new String[3];
-    private double result;
 
-    public double getResult() {
-        return result;
+    public double getNumber1() {
+        return number1;
     }
 
-    public double calculate(String mathematicalExpression) {
-        this.mathematicalExpression = mathematicalExpression;
-        assignValues();
+    public double getNumber2() {
+        return number2;
+    }
+
+    public char getSign() {
+        return sign;
+    }
+
+    public double calculate(String expression) {
+        perseExpression(expression);
         switch(sign) {
-            case '*' :
-                result = number1 * number2;
-                break;
-            case '+' :
-                result = number1 + number2;
-                break;
-            case '-' :
-                result = number1 - number2;
-                break;
-            case '/' :
-                result = number1 / number2;
-                break;
-            case '^' :
-                result = (int) Math.pow(number1, number2);
-                break;
-            case '%' :
-                result = number1 % number2;
+            case '*':
+                return number1 * number2;
+            case '+':
+                return number1 + number2;
+            case '-':
+                return number1 - number2;
+            case '/':
+                return number1 / number2;
+            case '^':
+                return (int) Math.pow(number1, number2);
+            case '%':
+                return number1 % number2;
         }
-        displayMathematicalExpression();
-        return result;
+        return sign;
     }
 
-    public void assignValues() {
-        data = mathematicalExpression.split(" ");
+    public void perseExpression(String expression) {
+        String[] data = new String[3];
+        data = expression.split(" ");
         number1 = Integer.parseInt(data[0]);
         sign = data[1].charAt(0);
         number2 = Integer.parseInt(data[2]);
-    }
-
-    public void displayMathematicalExpression() {
-        System.out.printf("%.0f %c %.0f = ", number1, sign, number2);
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,19 +3,18 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
+
     public static void main(String[] args) {
         String answer = "yes";
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-
         while (!"no".equals(answer)) {
             if ("yes".equals(answer)) {
                 System.out.println("Введите математическое выражение: ");
-                if (calculator.calculate(scanner.nextLine()) % 1 == 0) {
-                    System.out.printf("%.0f", calculator.getResult());
-                } else {
-                    System.out.printf("%.3f", calculator.getResult());
-                }
+                double result = calculator.calculate(scanner.nextLine());
+                System.out.printf("%.0f %c %.0f = ", calculator.getNumber1(), calculator.getSign(),
+                        calculator.getNumber2());
+                System.out.printf((result % 1 == 0 ? "%.0f" : "%.3f"), result);
             }
             System.out.println("\nХотите продолжить вычисления?[yes/no] :");
             answer = scanner.nextLine().toLowerCase();
