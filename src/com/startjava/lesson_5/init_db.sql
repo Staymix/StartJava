7960080e44069a5c55be43904a5ae4c3d5042601
Staymix <staszubov199530@gmail.com>
2023-03-11 17:38:10 +0600

    Для проверки

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,14 @@
+-- init database
+
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+       VALUES
+		('Brawler Yukon',    '1', '78.58', '2.124', 'Destroyed', 'United States of America', '2015-06-30', '2' ),
+		('Diablo Intercept', '2', '85.11', '3.159', 'Destroyed', 'Chile',                    '2016-01-01', '1' ),
+		('Gipsy Danger',     '3', '79.25', '1.980', 'Destroyed', 'United States of America', '2017-07-10', '9' ),
+		('Crimson Typhoon',  '4', '76.20', '1.722', 'Destroyed', 'China', 		     '2018-08-22', '7' ),
+		('Striker Eureka',   '5', '76.20', '1.850', 'Destroyed', 'Australia', 	             '2019-11-02', '11'),
+		('Coyote Tango',     '1', '85.34', '2.312', 'Destroyed', 'Japan', 		     '2015-12-30', '2' ),
+		('Solar Prophet',    '2', '99.01', '1.987', 'Destroyed', 'Peru', 		     '2016-01-01', '3' ),
+		('Matador Fury',     '3', '89.20', '2.314', 'Destroyed', 'Mexico', 		     '2017-09-14', '4' ),
+		('Hydra Corinthian', '4', '93.28', '1.651', 'Destroyed', 'United States of America', '2018-01-01', '5' ),
+		('Nova Hyperion',    '4', '86.01', '1.853', 'Destroyed', 'South Korea', 	     '2018-01-01', '6' );
---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,40 @@
+\echo 'Выводим всю таблицу'
+SELECT * FROM jaegers;
+
+\echo 'Отображаем только не уничтоженных роботов'
+SELECT * FROM jaegers 
+ WHERE status != 'Destroyed';
+
+\echo 'Отображаем роботов серий: Mark-1 и Mark-4'
+SELECT * FROM jaegers
+ WHERE mark IN ('1', '4');
+
+\echo 'Отображаем роботов кроме серий: Mark-1 и Mark-4'
+SELECT * FROM jaegers
+ WHERE mark NOT IN ('1', '4');
+
+\echo 'Сортируем таблицу по убыванию по столбцу mark'
+SELECT * FROM jaegers
+ ORDER BY mark DESC;
+
+\echo 'Отображаем информацию о самом старом роботе'
+SELECT * FROM jaegers
+ ORDER BY launch DESC
+ LIMIT 1;
+
+\echo 'Отображаем роботов, которые уничтожили больше всех kaiju'
+SELECT modelname
+  FROM jaegers
+ WHERE kaijukill = (SELECT MAX(kaijukill) FROM jaegers);
+
+\echo 'Отображаем средний вес роботов'
+SELECT AVG(weight) AS jaegersAvg FROM jaegers;
+
+\echo 'Увеличиваем на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены'
+UPDATE jaegers SET kaijukill = kaijukill + 1
+ WHERE status != 'Destroyed';
+
+\echo 'Удаляем уничтоженных роботов'
+DELETE FROM jaegers
+ WHERE status = 'Destroyed';
+
