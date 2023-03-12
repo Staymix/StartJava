82302c8d404b286b0175ed2372a693892bc359cf
Staymix <staszubov199530@gmail.com>
2023-03-12 14:28:11 +0600

    Для проверки

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,21 +1,20 @@
--- create database
 DROP DATABASE IF EXISTS jaegers;
 CREATE DATABASE Jaegers;
 
-\С jaegers;
+\c jaegers;
 
 DROP TABLE IF EXISTS jaegers;
 CREATE TABLE Jaegers (
-	id        SERIAL PRIMARY KEY,
-	modelName TEXT,
-	mark      INTEGER,
-	height    NUMERIC(4,2),
-	weight    NUMERIC(4,3),
-	status    TEXT,
-	origin    TEXT,
-	launch    DATE,
-	kaijuKill INTEGER
+  id        SERIAL PRIMARY KEY,
+  modelName TEXT,
+  mark      INTEGER,
+  height    NUMERIC(4,2),
+  weight    NUMERIC(4,3),
+  status    TEXT,
+  origin    TEXT,
+  launch    DATE,
+  kaijuKill INTEGER
 );
 
-\i D:/soft/java/study/StartJava/src/com/startjava/lesson_5/init_db.sql
-\i D:/soft/java/study/StartJava/src/com/startjava/lesson_5/queries.sql
+\i init_db.sql
+\i queries.sql
